f718a813d861d0c12ed43684a2b14f13dcf63261
apascualucsd <102634499+apascualucsd@users.noreply.github.com>
2024-05-01 16:19:48 -0700

    Update Tritone_Teensy.ino

--- Tritone_Teensy.ino
+++ Tritone_Teensy.ino
@@ -16,8 +16,13 @@ AudioConnection patchCord2(i2s1, 1, peak1, 0);
 // GUItool: end automatically generated code
 
 AudioControlSGTL5000 audioShield;
+unsigned long lastPeakCountTime = 0;  // Time of the last peak count
+unsigned int peakCount = 0;  // Number of peaks detected in the last second
+unsigned int bpm = 0;  // Beats per minute
 
 void setup() {
+  Serial.begin(9600);  // Initialize serial communication
+  while (!Serial);      // Wait for serial connection
    AudioMemory(30);
    audioShield.enable();
    audioShield.inputSelect(myInput);
@@ -33,10 +38,53 @@ void loop() {
       float val = fft.read(i);
       if (val > maxVal) {
         maxVal = val;
-        maxFreq = i * 44100 / 1024;
+        maxFreq = i * 44100 / 1024; //sampling rate (44100) and size of fft (1024)
+      //By dividing i by 1024, you get a value between 0 and 1
+      //representing the position of the bin in the frequency spectrum. 
+      //Multiplying this value by the sampling rate 44100 gives you the actual frequency in Hz that the bin represents. 
+      //This calculation allows you to determine the frequency of the audio signal corresponding to each bin in the FFT output.
       }
+
     }
-    float amplitude = peak1.read();
-    Serial.printf("Dominant Frequency: %.2f Hz | Amplitude: %.2f\n", maxFreq, amplitude);
+      float amplitude = peak1.read();
+      if(amplitude < 0.5){
+        amplitude = 0;
+      }
+
+      bpm = calculateBPM(amplitude);
+
+      String tempo;
+      if (bpm < 60) {
+        tempo = "Largo";
+      } else if (bpm < 76) {
+        tempo = "Adagio";
+      } else if (bpm < 108) {
+        tempo = "Moderato";
+      } else if (bpm < 120) {
+        tempo = "Allegro";
+      } else {
+        tempo = "Presto";
+      }
+    
+    Serial.println(amplitude);
+    Serial.printf("Dominant Frequency: %.2f Hz | Amplitude: %.2f | Tempo: %s | BPM: %d\n", maxFreq, amplitude, tempo.c_str(), bpm);
   }
+}
+
+int calculateBPM(float amplitude) {
+  // Count peaks in the last second
+  unsigned long currentTime = millis();
+  if (currentTime - lastPeakCountTime == 1000) {
+    // One second has elapsed, calculate BPM
+    bpm = peakCount*60;
+    // Reset peak count and update lastPeakCountTime
+    peakCount = 0;
+    lastPeakCountTime = currentTime;
+  }
+
+  // Detect peak
+  if(amplitude >= 0.5){ //fine tune 
+    peakCount++;
+  }
+  return bpm;
 }
